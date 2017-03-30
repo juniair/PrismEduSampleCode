aa7be197fc20d379cb66f583f4b5f1abe9885731
juniair <juniair.kim@gmail.com>
2017-03-30 15:55:37 +0900

    Upload CollectionView sample

---
+++ Usging-ICollectionChanged/CollectionSampleApp/CollectionSampleApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CollectionSampleApp.Model;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace CollectionSampleApp.ViewModels
+{
+    public class MainWindowViewModel : BindableBase, INotifyCollectionChanged
+    {
+        private string _title = "Prism Unity Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+
+        private ObservableCollection<Person> myData;
+
+        /// <summary>
+        /// Gets the list of persons to show in the UI
+        /// </summary>
+        public ObservableCollection<Person> MyData
+        {
+            get { return myData; }
+        }
+
+        SampleModel model;
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        
+        public MainWindowViewModel()
+        {
+            this.model = new SampleModel();
+
+            myData = new ObservableCollection<Person>(model.GenerateSampleData());
+
+        }
+
+    }
+}
